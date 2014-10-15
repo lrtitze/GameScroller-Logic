741b5aeea2bd689983645506ec631b3e9b76566a
Leslie Titze <lrtitze@mac.com>
2014-10-15 14:34:19 -0500

    Basic SimulationView class
    Motion of the character is not constrained at all.

--- GScroller/SimulationView.swift
+++ GScroller/SimulationView.swift
@@ -6,4 +6,24 @@
 //  Copyright (c) 2014 Leslie Titze. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class SimulationView: UIView {
+  var bgXOffset   = CGFloat(0.0)
+  var bgYOffset   = CGFloat(0.0)
+  var charXOffset = CGFloat(10.0)
+  var charYOffset = CGFloat(10.0)
+
+  override func drawRect(rect: CGRect) {
+    Simulator.drawSimulation(bgXOffset, backgroundYOffset: bgYOffset, characterXOffset: charXOffset, characterYOffset: charYOffset)
+  }
+
+
+  func moveBy(x:CGFloat, y:CGFloat) {
+    charXOffset += x
+    charYOffset += y
+
+    setNeedsDisplay();
+  }
+}
