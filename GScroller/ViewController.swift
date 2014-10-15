9983e292af61b5a5a170b8a0aaf5196da3bd3eb4
Leslie Titze <lrtitze@mac.com>
2014-10-15 14:33:25 -0500

    Added Pan gesture handler

--- GScroller/ViewController.swift
+++ GScroller/ViewController.swift
@@ -22,6 +22,13 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+    let translation = recognizer.translationInView(self.view)
+    //recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x, y:recognizer.view!.center.y + translation.y)
+    simulationView?.moveBy(translation.x, y: translation.y)
+
+    recognizer.setTranslation(CGPointZero, inView: self.view)
+  }
 
 }
 
