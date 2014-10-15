f18d56d3540602ef27ef844caaeaf1c536ef6978
Leslie Titze <lrtitze@mac.com>
2014-10-14 21:44:15 -0500

    Initial Commit

---
+++ GScroller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GScroller
+//
+//  Created by Leslie Titze on 2014.10.14.
+//  Copyright (c) 2014 Leslie Titze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
